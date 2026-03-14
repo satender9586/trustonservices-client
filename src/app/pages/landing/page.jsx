20f35f604bd55cdd420a08a788943b1d3d874264
satender <satenderahirwar.corporate@gmail.com>
2026-03-14 23:20:02 +0530

    feature/navbar-ui-componets-added

--- src/app/pages/landing/page.jsx
+++ src/app/pages/landing/page.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '@/app/components/layout/Navbar'
 import HeroSection from './components/HeroSection'
+import Footer from '@/app/components/layout/Footer'
+import TestomonialSection from './components/TestomonialSection'
 
 const Landing = () => {
   return (
     <>
         <Navbar/>
         <HeroSection/>
+        <TestomonialSection/>
+        <Footer/>
     </>
   )
 }
