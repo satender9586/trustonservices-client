1bb492d38fc014fd23fdc0f42e3922f65ac7162e
satender kumar <sksatenderkumar59@gmail.com>
2026-03-14 22:21:27 +0530

    feature/navbar-ui-components-added

---
+++ src/app/pages/landing/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '@/app/components/layout/Navbar'
+import HeroSection from './components/HeroSection'
+
+const Landing = () => {
+  return (
+    <>
+        <Navbar/>
+        <HeroSection/>
+    </>
+  )
+}
+
+export default Landing
